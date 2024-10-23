97d6487cd2061a2c55da9966b4a8f3d76a9f1b34
suhokym <suhokym@gmail.com>
2024-10-23 11:59:34 +0900

    div 영역

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as  Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home.jsx";
 import Navbar from "./components/Navbar.jsx";
 
