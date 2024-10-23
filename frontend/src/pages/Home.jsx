4f07bb5f89e9a404bc6847515793b45e7a6a20eb
suhokym <suhokym@gmail.com>
2024-10-23 12:05:50 +0900

    div 영역

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -14,9 +14,18 @@ import 'swiper/css/effect-coverflow';
 const Home = () => {
 
     return (
-        <div className="container mx-auto p-8">  {/*슬라이더 박스*/}
-
+        <div className="flex flex-col h-full items-center justify-center mx-56">  {/*슬라이더 박스*/}
+            <div className="flex flex-row  h-14 w-[35rem] items-center justify-center bg-red-500">
+                검색 div
+            </div>
+            <div className="flex flex-row w-[60rem] h-[20rem] mt-6 items-center justify-center bg-indigo-700">
+                광고
+            </div>
+            <div className="flex flex-row w-[60rem] h-[13rem] mt-6 items-center justify-center bg-indigo-300">
+                제품
+            </div>
         </div>
+
     )
 }
 
