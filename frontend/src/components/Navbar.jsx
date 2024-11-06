2109857ac89880d09c356ab5dca051ca0519c6ea
suhokym <suhokym@gmail.com>
2024-11-06 12:16:34 +0900

    Product_Main.jsx 추가

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,98 +1,99 @@
-        import {Link, useNavigate} from "react-router-dom";
-        import React, {useEffect, useState} from 'react';
-        import {useRecoilValue} from "recoil";
-        import {Button} from "@headlessui/react";
-        import { IoPersonOutline } from "react-icons/io5";
-        const Navbar = () => {
-            const navigate = useNavigate();
-            const [Login,setLogin] = useState("Login");
-            const handleClick = () => {
-                navigate(`/`);
-            };
+import { Link, useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import { Button } from "@headlessui/react";
+import { IoPersonOutline } from "react-icons/io5";
 
-            const changeLogin = () => {
-                if(Login == "Login"){
-                    setLogin("Logout")
-                }else if(Login == "Logout"){
+const Navbar = () => {
+    const navigate = useNavigate();
+    const [Login, setLogin] = useState("Login");
+    const [profile, setprofile] = useState("https://ifh.cc/g/bFHjG0.png");
+    // 홈으로 이동하는 함수
+    const handleClick = () => {
+        navigate(`/`);  // 라우터 경로로 이동
+    };
 
-                }
+    // 로그인 상태를 토글하는 함수
+    const changeLogin = () => {
+        if (Login === "Login") {
+            setprofile("https://ifh.cc/g/bFHjG0.png")
+            setLogin("Logout");
+        } else if (Login === "Logout") {
+            setprofile("https://ifh.cc/g/7ky5bT.jpg")
+            setLogin("Login");
+        }
+    };
 
-
-            };
-
-            return (
-                <div className="navbar bg-base-100">
-                    <div className="navbar-start">
-                        <div className="drawer">
-                            <input id="my-drawer" type="checkbox" className="drawer-toggle"/>
-                            <div className="drawer-content">
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="drawer">
+                    <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+                    <div className="drawer-content">
+                        {/* 메뉴 버튼 */}
+                        <label htmlFor="my-drawer" className="btn btn-square btn-ghost drawer-button">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                 className="inline-block w-5 h-5 stroke-current">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                      d="M4 6h16M4 12h16M4 18h16"></path>
+                            </svg>
+                        </label>
+                    </div>
+                    <div className="drawer-side z-10">
+                        <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+                        <ul className="menu p-4 w-60 min-h-full bg-base-200 text-base-content">
+                            {/* 사이드바 내용 */}
+                            <div className="drawer-content items-center flex flex-row">
                                 {/* 메뉴 버튼 */}
                                 <label htmlFor="my-drawer" className="btn btn-square btn-ghost drawer-button">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                          className="inline-block w-5 h-5 stroke-current">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                                               d="M4 6h16M4 12h16M4 18h16"></path>
                                     </svg>
                                 </label>
+                                <a><img className="mt-1 ml-2 w-28 mx-auto p-auto" alt="로고"
+                                        src="https://ifh.cc/g/VRak2k.png" /></a>
                             </div>
-                            <div className="drawer-side z-10">
-                                <label htmlFor="my-drawer" aria-label="close sidebar"
-                                       className="drawer-overlay"></label>
-                                <ul className="menu p-4 w-60 min-h-full bg-base-200 text-base-content">
-                                    {/* 사이드바 내용 */}
-                                    <div className="drawer-content items-center flex flex-row">
-                                        {/* 메뉴 버튼 */}
-                                        <label htmlFor="my-drawer" className="btn btn-square btn-ghost drawer-button">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                                 className="inline-block w-5 h-5 stroke-current">
-                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                      d="M4 6h16M4 12h16M4 18h16"></path>
-                                            </svg>
-                                        </label>
-                                        <a><img className="mt-1 ml-2 w-28 mx-auto p-auto" alt="로고"
-                                                src="https://ifh.cc/g/VRak2k.png"/></a>
-                                    </div>
-                                    <li className="flex-row" onClick={handleClick}>
-                                        <a>
-                                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none"
-                                                 viewBox="0 0 24 24" stroke="currentColor">
-                                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                                      d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"/>
-                                            </svg>
-                                            <p className="items-center font-normal ml-[0.8rem]">홈</p>
-                                        </a></li>
-                                </ul>
-                            </div>
-                        </div>
+                            <li className="flex-row" onClick={handleClick}>
+                                <a>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none"
+                                         viewBox="0 0 24 24" stroke="currentColor">
+                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                                              d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+                                    </svg>
+                                    <p className="items-center font-normal ml-[0.8rem]">홈</p>
+                                </a>
+                            </li>
+                        </ul>
                     </div>
-                    <div className="navbar-center">
-                    <a className="btn btn-ghost text-xl">
-                    <img alt="로고" src="https://ifh.cc/g/VRak2k.png"/></a>
+                </div>
+            </div>
+            <div className="navbar-center">
+                <a className="btn btn-ghost text-xl">
+                    <img alt="로고" src="https://ifh.cc/g/VRak2k.png" />
+                </a>
             </div>
             <div className="navbar-end">
                 <div className="dropdown dropdown-end">
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full">
-                            <img alt="프로필 사진"
-                                 src="https://ifh.cc/g/7ky5bT.jpg"/>
-                                </div>
-                            </div>
-                        <ul tabIndex={0}
-                            className="mt-3 z-10 p-2 shadow-md menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
-                            <li>
-                                <a className="justify-between font-bold">
-                                    Profile
-                                </a>
-                            </li>
-                            <li><a className="font-bold">Settings</a></li>
-
-                            <li><a className="font-bold">{Login}</a></li>
-                        </ul>
+                            <img alt="프로필 사진" src={profile} />
+                        </div>
                     </div>
-                    </div>
+                    <ul tabIndex={0}
+                        className="mt-3 z-10 p-2 shadow-md menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                        <li>
+                            <a className="justify-between font-bold">
+                                Profile
+                            </a>
+                        </li>
+                        <li><a className="font-bold">Settings</a></li>
+                        <li><a onClick={changeLogin} className="font-bold">{Login}</a></li>
+                    </ul>
                 </div>
-            )
+            </div>
+        </div>
+    );
+};
 
-        };
-
-        export default Navbar;
+export default Navbar;
---
+++ frontend/src/pages/Product.jsx
@@ -0,0 +1,38 @@
+import React, {useState, useEffect} from "react";
+import {Link} from "react-router-dom";
+import {useRecoilValue} from "recoil";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, A11y, Autoplay, EffectCoverflow } from "swiper";
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-coverflow';
+
+
+
+const Product= () => {
+
+    return (
+        <div className="flex flex-col h-full items-center justify-center mx-44  ">  {/*슬라이더 박스*/}
+            <div className="flex flex-row  h-14 w-[35rem] items-center justify-center bg-red-500">
+                검색 div
+            </div>
+            <div className="flex justify-start flex-row w-[62rem] h-[22rem] mt-6 items-center">
+                <div className="flex flex-row bg-indigo-700  w-[33rem] h-[22rem]">그림 부분</div>
+                <div className="flex flex-row bg-green-300  w-[29rem] h-[22rem]">운동 소개 부분</div>
+            </div>
+            <div className="flex flex-row justify-start bg-violet-300 w-[62rem] h-[35rem] mt-14 justify-start">
+
+
+
+
+            </div>
+
+        </div>
+
+    )
+}
+
+export default Product;
