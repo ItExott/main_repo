738b13ff4f4561dd506e519db46873ac07d7c6a9
heeseokid <heeseokid@g.shingu.ac.kr>
2024-11-18 20:22:15 +0900

    Product 상세 페이지 디자인 수정중2

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -12,6 +12,7 @@ import { PiLockersFill } from "react-icons/pi";
 import { FaCircleUser } from "react-icons/fa6";
 import { TiArrowSortedDown } from "react-icons/ti";
 import { TiArrowSortedUp } from "react-icons/ti";
+import { MdDateRange } from "react-icons/md";
 import BuyBox from "../components/BuyBox.jsx";
 
 const Product= () => {
@@ -144,45 +145,48 @@ const Product= () => {
                         </div>
                     </div>
                     </div>
-
-
-                    <div className="flex divider divide-gray-600 ml-[4rem] w-[60rem]"></div>
-
+                    <div className="flex divide-gray-600 ml-[4rem] w-[60rem]"></div>
                 <div className="flex flex-col"> {/*하단 소개글*/}
-                    <div
-                        className="flex text-sm mx-auto flex-col bg-gray-300 shadow-xl border-[0.1rem] border-gray-600 items-center justify-center rounded-lg w-[50rem] h-[9rem]">
-                        <a className="text-lg font-bold">마천동 Mining 클라이밍짐</a>
-                        <div className="flex flex-col w-1/2">
+                    <div className="flex flex-row text-sm mx-auto mt-[2rem] bg-gray-300 shadow-xl border-[0.1rem] border-gray-600 items-center justify-center rounded-lg w-[50rem] h-[20rem]">
+                        <div className="flex flex-col rounded-3xl bg-white justify-center items-center h-[18rem] w-[18rem]">
+                            <a className="flex text-sm bg-yellow-200 border-[0.2rem] border-yellow-200 items-center justify-center rounded-xl w-[12rem] font-bold mt-[1.2rem]">Mining 클라이밍</a>
+                            <div className="flex items-center justify-center flex-col w-1/2">
+                                <div className="flex w-[8rem] mt-[1rem] h-[8rem]"><img className="rounded-full"
+                                                                                         src="https://ifh.cc/g/XpkHf4.jpg"></img></div>
+                                <div className="flex flex-col items-center justify-center bg-yellow-200 mt-[1rem] rounded-xl h-[5rem] w-[20rem]">
+                                <a className="text-xs font-semibold whitespace-nowrap">안녕하세요! Mining 클라이밍짐를 소개합니다!</a>
+                                <a className="text-xs whitespace-nowrap">퍼즐처럼 예쁜 벽에서 등반하고 싶다면,</a>
+                                <a className="text-xs whitespace-nowrap">바로 여기 Mining 클라이밍짐으로 오시면 됩니다!</a>
+                                    <a className="text-xs whitespace-nowrap">Mining 클라이밍 짐에서 새로운 도전을 시작해 보세요!</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="border-l border-gray-400 ml-[3.5rem] h-[16rem]"></div>
+                        <div className="flex flex-col ml-[3.5rem] rounded-3xl bg-white items-center w-[18rem] h-[18rem]">
+                            <a className="flex text-sm bg-yellow-200 border-[0.2rem] border-yellow-200 items-center justify-center rounded-xl w-[12rem] font-bold mt-[1.2rem]">운영
+                                시간 및 이용 안내</a>
+                            <div className="flex flex-row justify-center mt-[2.8rem] w-[18rem] h-[18rem]">
+                                <div ref={RestSectionRef} className="flex ml-[0.5rem] items-center flex-col w-1/2">
+                                    <MdDateRange className="flex w-[3rem] h-[3rem]"/>
+                                    <a className="flex font-semibold mt-[0.5rem]">[ 평일 ]</a>
+                                    <a className="flex text-xs whitespace-nowrap">11:00 ~ 22:00</a>
+                                    <a className="font-semibold mt-[0.5rem] whitespace-nowrap">[ 주말 및 공휴일 ]</a>
+                                    <a className="whitespace-nowrap text-xs">11:00 ~ 20:00</a>
+                                </div>
+                                <div className="flex flex-col items-center mr-[0.5rem] w-1/2">
+                                    <FaParking className="flex w-[3rem] h-[3rem]"/>
+                                    <a className="font-semibold mt-[0.5rem]">[ 주차 ]</a>
+                                    <a className="flex text-xs whitespace-nowrap">1시간 무료</a>
+                                    <a className="flex text-xs whitespace-nowrap">이후 시간당 1000원</a>
 
-                        </div>
-                        <div className="flex flex-col w-1/2">
-                        <a className="flex mt-[1rem]">안녕하세요! Mining 클라이밍짐를 소개합니다!</a>
-                        <a>퍼즐처럼 예쁜 벽에서 등반하고 싶다면, 바로 여기 Mining 클라이밍짐으로 오시면 됩니다!</a>
-                        <a>Mining 클라이밍 짐에서 새로운 도전을 시작해 보세요!</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div  ref={RestSectionRef} className="flex divider divide-gray-600 ml-[4rem] w-[60rem]"></div>
-                <div
-                    className="flex mx-auto text-sm flex-col bg-gray-300 shadow-xl border-[0.1rem] border-gray-600 items-center justify-center rounded-lg w-[50rem] h-[9rem]">
-                    <a className="font-bold text-lg">운영 시간 및 이용 안내</a>
-                    <div className="flex flex-row ml-[5.4rem]">
-                        <div className="flex flex-col">
-                            <a>[ 평 일 ]</a>
-                            <a>11:00~22:00</a>
-                        </div>
-                        <div className="flex flex-col">
-                            <a className="ml-[4rem]">[ 주 차 ]</a>
-                            <a className="ml-[3rem]">1시간 무료</a>
-                            <a className="ml-[3rem]">1시간 이후 시간당 1000원</a>
-                        </div>
-                    </div>
-                    <a className="mr-[9rem]">[ 주말 및 공휴일 ]</a>
-                    <a className="mr-[9rem]">11:00~20:00</a>
-                </div>
-                <div className="flex divider divide-gray-600 ml-[4rem] w-[60rem]"></div>
-                <div
-                     className="flex mx-auto flex-col bg-gray-300 shadow-xl border-[0.1rem] border-gray-600 rounded-lg w-[50rem] h-[9rem]">
+                    <div className="flex divider divide-gray-600 ml-[4rem] w-[60rem]"></div>
+                    <div
+                        className="flex mx-auto flex-col bg-gray-300 shadow-xl border-[0.1rem] border-gray-600 rounded-lg w-[50rem] h-[9rem]">
                     <a className="text-lg font-bold mt-[0.5rem] ml-[1rem]">편의 시설</a>
                     <div className="flex flex-row">
                         <div className="flex flex-col">
@@ -205,13 +209,13 @@ const Product= () => {
                             <FaParking className="mt-[0.5rem] ml-[2.5rem] w-[3rem] h-[3rem]"/>
                             <a className="font-bold text-sm mt-[0.4rem] ml-[3rem]">주차</a>
                         </div>
-                        <div className="flex flex-col">
+                        <div  ref={PhotoSectionRef} className="flex flex-col">
                             <PiLockersFill className="mt-[0.5rem] ml-[2.5rem] w-[3rem] h-[3rem]"/>
                             <a className="font-bold text-sm mt-[0.4rem] ml-[2.2rem]">개인락커</a>
                         </div>
                     </div>
                 </div>
-                <div ref={PhotoSectionRef} className="flex divider divide-gray-600 ml-[4rem] w-[60rem]"></div>
+                <div  className="flex divider divide-gray-600 ml-[4rem] w-[60rem]"></div>
                 <a className="flex text-lg font-bold">시설사진</a>
                 <div className="flex flex-row space-x-7 mt-[1.5rem]">{/*시설사진*/}
                     <div className="flex w-[11rem] h-[10rem] hover:scale-125 transition-transform ease-in-out duration-500"><img className="rounded-xl" src="https://ifh.cc/g/CoJTAC.png"/></div>
