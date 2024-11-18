7b53156257bbe6024bad45a6e3200e7e7c10e31e
suhokym <suhokym@gmail.com>
2024-11-18 09:26:20 +0900

    출석체크

--- frontend/src/popup/Attendance.jsx
+++ frontend/src/popup/Attendance.jsx
@@ -1,5 +1,3 @@
-// src/components/AttendancePopup.jsx
-
 import React, { useState, useEffect } from "react";
 import Calendar from "react-calendar";
 import 'react-calendar/dist/Calendar.css';
@@ -24,12 +22,18 @@ const Attendance = ({ isOpen, onClose, onDateSelect }) => {
 
     return (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-            <div className="bg-white p-6 rounded-xl shadow-lg relative">
-                <h3 className="text-xl font-bold mb-4">출석체크</h3>
-                <Calendar
-                    onChange={handleDateChange}
-                    value={selectedDate}
-                />
+            <div className="bg-white p-4 w-[80rem] h-[40rem] max-w-4xl rounded-xl shadow-2xl relative flex flex-col justify-between items-center overflow-hidden">
+                <h3 className="text-xl font-bold mb-6 absolute top-4 left-1/2 transform -translate-x-1/2">출석체크</h3>
+
+                {/* 캘린더 래퍼 */}
+                <div className="flex-grow w-full flex justify-center items-center overflow-auto">
+                    <Calendar
+                        onChange={handleDateChange}
+                        value={selectedDate}
+                        className="react-calendar w-full h-full custom-calendar"  // 커스터마이징된 클래스 추가
+                    />
+                </div>
+
                 <button
                     className="absolute top-2 right-2 text-xl"
                     onClick={onClose} // 팝업 닫기
