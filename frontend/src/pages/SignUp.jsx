6da80ccbdf2018db69ef9508f8e0dc30e2666f4d
suhokym <suhokym@gmail.com>
2024-11-18 22:43:34 +0900

    회원가입변경

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -57,7 +57,7 @@ const SignUp = () => {
         }
 
         // 휴대폰 번호 체크 (010-xxxx-xxxx 형태)
-        const phoneRegEx = /^\d{3}-\d{4}-\d{4}$/;
+        const phoneRegEx = /^\d{8}$/;
         if (formData.phoneNumber && !phoneRegEx.test(formData.phoneNumber)) {
             newErrors.phoneNumber = "올바른 휴대전화 번호 형식을 입력하세요. (예: 010-1234-5678)";
         }
@@ -92,153 +92,174 @@ const SignUp = () => {
             });
     };
 
+
     return (
-        <div className="flex flex-col h-full items-center justify-center mx-56">
-            <div className="flex flex-row mt-6 h-11 w-[35rem] items-center justify-center text-xl">
-                회원가입
-            </div>
-            <form onSubmit={handleSubmit}>
-                <div className="flex flex-col w-[60rem] h-auto mt-6 p-4">
-                    <div className="mb-4 p-4 items-start">
-                        <div className="text-left">회원구분</div>
-                        <div className="flex flex-row items-start mt-2">
-                            <label className="mr-4">
-                                <input
-                                    type="radio"
-                                    name="userType"
-                                    value="individual"
-                                    checked={formData.userType === "individual"}
-                                    onChange={handleChange}
-                                />
-                                개인회원
-                            </label>
-                            <label>
-                                <input
-                                    type="radio"
-                                    name="userType"
-                                    value="gymManager"
-                                    checked={formData.userType === "gymManager"}
-                                    onChange={handleChange}
-                                />
-                                GYM 관리자
-                            </label>
+        <div className="flex flex-col h-full items-center justify-center bg-gray-300 mx-[24rem] rounded-3xl ">
+
+
+            <div className="flex flex-col mb-[2rem] mt-[2rem] h-full items-center justify-center bg-white w-[65rem]  rounded-3xl ">
+                <div
+                    className="border-b-[0.1rem] border-gray-950 mb-4 font-bold  flex flex-row mt-6 h-11 w-[35rem] items-center justify-center text-xl">
+
+                    <a className="mb-[1rem] font-bold">회원가입</a>
+                </div>
+                <ul className="w-[45rem] steps">
+                    <li className=" step step-neutral">Register</li>
+                    <li className="step step-neutral">Choose plan</li>
+                    <li className="step">Purchase</li>
+                    <li className="step">Receive Product</li>
+                </ul>
+                <div className="flex w-[40rem] items-center ">
+                <form onSubmit={handleSubmit}>
+
+                    <div className="flex flex-col w-[60rem] h-auto mt-6 p-4">
+                        <div className="mb-4 items-start">
+                            <div className="text-left ">회원구분</div>
+                            <div className="flex flex-row items-start mt-2">
+                                <label className="mr-4">
+                                    <input type="radio"
+                                           name="userType"
+                                           className="radio"
+                                           value="individual"
+                                           checked={formData.userType === "individual"}
+                                           onChange={handleChange} defaultChecked/>
+
+
+                                    <a className="text-xs ml-[0.2rem]" > 개인회원</a>
+                                </label>
+                                <label>
+                                    <input type="radio"
+                                           name="userType"
+                                           value="gymManager"
+                                           className="radio "
+                                           onChange={handleChange}
+                                           checked={formData.userType === "gymManager"}/>
+
+                                    <a className="text-xs  ml-[0.2rem]" > GYM 관리자</a>
+                                </label>
+                            </div>
                         </div>
-                    </div>
 
-                    <div className="mb-4 mt-3">
-                        <label>이름</label>
-                        <input
-                            type="text"
-                            name="name"
-                            value={formData.name}
-                            onChange={handleChange}
-                            required
-                            className="p-2 w-full"
-                        />
-                        {errors.name && <p className="text-red-500">{errors.name}</p>}
-                    </div>
-
-                    <div className="mb-4 mt-3">
-                        <label>사용자 아이디 (로그인 아이디)</label>
-                        <input
-                            type="text"
-                            name="userId"
-                            value={formData.userId}
-                            onChange={handleChange}
-                            required
-                            className="p-2 w-full"
-                        />
-                        {errors.userId && <p className="text-red-500">{errors.userId}</p>}
-                    </div>
-
-                    <div className="mb-4">
-                        <label>비밀번호</label>
-                        <input
-                            type="password"
-                            name="password"
-                            value={formData.password}
-                            onChange={handleChange}
-                            required
-                            className="p-2 w-full"
-                        />
-                        {errors.password && <p className="text-red-500">{errors.password}</p>}
-                    </div>
-
-                    <div className="mb-4">
-                        <label>비밀번호 확인</label>
-                        <input
-                            type="password"
-                            name="passwordConfirm"
-                            value={formData.passwordConfirm}
-                            onChange={handleChange}
-                            required
-                            className="p-2 w-full"
-                        />
-                        {errors.passwordConfirm && <p className="text-red-500">{errors.passwordConfirm}</p>}
-                    </div>
-
-                    <div className="mb-4">
-                        <label>주소</label>
-                        <input
-                            type="text"
-                            name="address"
-                            value={formData.address}
-                            onChange={handleChange}
-                            required
-                            className="p-2 w-full"
-                        />
-                        {errors.address && <p className="text-red-500">{errors.address}</p>}
-                    </div>
-
-                    <div className="mb-4">
-                        <label>휴대전화</label>
-                        <div className="flex">
-                            <select
-                                name="phonePrefix"
-                                value={formData.phonePrefix}
-                                onChange={handlePhonePrefixChange}
-                                className="p-2 w-24"
-                            >
-                                <option value="010">010</option>
-                                <option value="011">011</option>
-                                <option value="016">016</option>
-                                <option value="017">017</option>
-                                <option value="018">018</option>
-                                <option value="019">019</option>
-                            </select>
+                        <div className="mb-1 mt-3">
                             <input
+                                placeholder="이름"
                                 type="text"
-                                name="phoneNumber"
-                                value={formData.phoneNumber}
+                                name="name"
+                                value={formData.name}
                                 onChange={handleChange}
                                 required
-                                maxLength="13"
-                                placeholder="1234-5678"
-                                className="p-2 w-full ml-2"
+                                className="flex border-2 border-gray-400 text-sm font-normal w-[20rem] h-[3rem] mt-[0.2rem] placeholder-gray-300 "
+                            />
+                            {errors.name && <p className="text-red-500">{errors.name}</p>}
+                        </div>
+
+                        <div className="mb-2 mt-1">
+
+                            <input
+                                placeholder="아이디"
+                                type="text"
+                                name="userId"
+                                value={formData.userId}
+                                onChange={handleChange}
+                                required
+                                className="flex border-2 border-gray-400 text-sm font-normal w-[20rem] h-[3rem] mt-[0.2rem] placeholder-gray-300"
+                            />
+                            {errors.userId && <p className="text-red-500">{errors.userId}</p>}
+                        </div>
+
+                        <div className="mb-2">
+
+                            <input
+                                placeholder="비밀번호"
+                                type="password"
+                                name="password"
+                                value={formData.password}
+                                onChange={handleChange}
+                                required
+                                className="flex border-2 border-gray-400 text-sm font-normal w-[20rem] h-[3rem] mt-[0.2rem] placeholder-gray-300 "
+                            />
+                            {errors.password && <p className="text-red-500">{errors.password}</p>}
+                        </div>
+
+                        <div className="mb-4">
+
+                            <input
+                                placeholder="비밀번호 확인"
+                                type="password"
+                                name="passwordConfirm"
+                                value={formData.passwordConfirm}
+                                onChange={handleChange}
+                                required
+                                className="flex border-2 border-gray-400 text-sm font-normal w-[20rem] h-[3rem] mt-[0.2rem] placeholder-gray-300 "
+                            />
+                            {errors.passwordConfirm && <p className="text-red-500">{errors.passwordConfirm}</p>}
+                        </div>
+
+                        <div className="mb-4">
+
+                            <input
+                                placeholder="주소"
+                                type="text"
+                                name="address"
+                                value={formData.address}
+                                onChange={handleChange}
+                                required
+                                className="flex border-2 border-gray-400 text-sm font-normal w-[20rem] h-[3rem] mt-[0.2rem] placeholder-gray-300 "
+                            />
+                            {errors.address && <p className="text-red-500">{errors.address}</p>}
+                        </div>
+
+                        <div className="mb-4">
+
+                            <div className="flex ">
+                                <select
+                                    name="phonePrefix"
+                                    value={formData.phonePrefix}
+                                    onChange={handlePhonePrefixChange}
+                                    className="flex  border-2 border-gray-400 text-sm font-normal w-[5rem] h-[3rem] mt-[0.2rem] placeholder-gray-300 "
+                                >
+                                    <option value="010">010</option>
+                                    <option value="011">011</option>
+                                    <option value="016">016</option>
+                                    <option value="017">017</option>
+                                    <option value="018">018</option>
+                                    <option value="019">019</option>
+                                </select>
+                                <input
+                                    type="text"
+                                    name="phoneNumber"
+                                    value={formData.phoneNumber}
+                                    onChange={handleChange}
+                                    required
+                                    maxLength="13"
+                                    placeholder="1234-5678"
+                                    className="flex ml-[0.5rem] border-2 border-gray-400 text-sm font-normal w-[15rem] h-[3rem] mt-[0.2rem] placeholder-gray-300 "
+                                />
+                            </div>
+                            {errors.phoneNumber && <p className="text-red-500">{errors.phoneNumber}</p>}
+                        </div>
+
+                        {/* 이메일은 필수 항목에서 제외 */}
+                        <div className="mb-4">
+                            <input
+                                placeholder="이메일"
+                                type="email"
+                                name="email"
+                                value={formData.email}
+                                onChange={handleChange}
+                                className="flex border-2 border-gray-400 text-sm font-normal w-[20rem] h-[3rem] mt-[0.2rem] placeholder-gray-300"
                             />
                         </div>
-                        {errors.phoneNumber && <p className="text-red-500">{errors.phoneNumber}</p>}
                     </div>
 
-                    {/* 이메일은 필수 항목에서 제외 */}
-                    <div className="mb-4">
-                        <label>이메일</label>
-                        <input
-                            type="email"
-                            name="email"
-                            value={formData.email}
-                            onChange={handleChange}
-                            className="p-2 w-full"
-                        />
+                    <div className="flex flex-row w-full h-[7rem] items-center justify-center">
+                        <button type="submit" className="w-40 h-12 bg-black text-white">
+                            회원가입
+                        </button>
                     </div>
+                </form>
                 </div>
-
-                <div className="flex flex-row w-full h-[7rem] items-center justify-center">
-                    <button type="submit" className="w-40 h-12 bg-black text-white">
-                        회원가입
-                    </button>
-                </div>
-            </form>
+            </div>
         </div>
     );
 };
