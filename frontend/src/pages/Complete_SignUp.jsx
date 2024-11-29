33543901b8d8d1badd8e642d5aa5a2ca1f96a3c1
suhokym <suhokym@gmail.com>
2024-11-29 23:28:53 +0900

    회원가입완료페이지 작성

--- frontend/src/pages/Complete_SignUp.jsx
+++ frontend/src/pages/Complete_SignUp.jsx
@@ -13,23 +13,26 @@ const Complete_SignUp = () => {
 
     return (
         <div className="flex flex-col h-full items-center justify-center mx-44">
-            <div className="flex flex-col justify-start w-[28rem] h-[27rem] mb-6 mt-40 items-center">
+            <div className="flex flex-col justify-start w-[28rem] h-[40rem] mb-6 mt-[4rem] items-center">
+                <h2 className="mb-5 font-bold font-sans text-3xl">가입완료</h2>
                 <ul className="w-[45rem] steps">
                     <li className=" step step-error">약관동의</li>
                     <li className="step step-error">본인인증</li>
                     <li className="step step-error">정보입력</li>
                     <li className="step step-error">가입완료</li>
                 </ul>
-                <h2 className="mb-9 font-bold font-sans text-3xl">Complete</h2>
-                <CiCircleCheck size="150" className="ml-3 cursor-pointer mt-[0.06rem]"/>
-                <h2 className="flex mt-4 mb-2 font-bold text-2xl">회원가입이 완료 되었습니다.</h2>
-                <h2 className="mb-14">{name}님 가입을 축하드립니다</h2>
-                <button
-                    className="bg-black text-white font-bold py-2 px-12 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-                    onClick={handleLoginRedirect}
-                >
-                    LOGIN
-                </button>
+                <div className="flex items-center justify-center flex-col mt-[2rem]">
+
+                    <img className="flex mb-[0.5rem]" src="/IMG/Complete_ICON.png"></img>
+                    <h2 className="flex mt-4 mb-2 font-bold text-2xl">회원가입이 완료 되었습니다.</h2>
+                    <h2 className="mb-14">{name}님 가입을 축하드립니다</h2>
+                    <button
+                        className="bg-red-400 text-white font-bold py-2 px-12 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+                        onClick={handleLoginRedirect}
+                    >
+                        LOGIN
+                    </button>
+                </div>
             </div>
         </div>
     );
